99e9a56242eb82cabd5b9f0706a2bdd156ace336
aabdishukurov <115713846+aabdishukurov@users.noreply.github.com>
2023-02-07 21:35:39 +0600

    DZ4

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { createContext, useState } from "react";
 import TodoPage from "./page/TodoPage";
 import StyleProvider from "./providers/StyleProvider";
 
+export const Context = createContext();
+
 function App() {
+  const [value, setValue] = useState(0);
+  const [search, setSearch] = useState("");
+  
   return (
-    <div className="App">
-      <StyleProvider>
-        <TodoPage/>
-      </StyleProvider>
-    </div>
+    <Context.Provider value={{value, setValue, search, setSearch}}>
+      <div>
+        <StyleProvider>
+          <TodoPage/>
+        </StyleProvider>
+      </div>
+    </Context.Provider>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+const obj = {
+  John: 19;
+  Adil: 23;
+  Islam: 20;
+  "name", "age"
+}
+
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,14 +1,21 @@
+import React, {useContext} from 'react'
 import TodoCard from "./TodoCard"
+import { Context } from '../App'
 
 import classes from './components.module.css'
 
 const TodoList = ({ todoList, editTodo, deleteTodo, completedOnChange }) => {
+  const {search} = useContext(Context)
 
   return (
     <div className={classes.flexList}>
-      {todoList.map((todo, i) => 
-        <TodoCard key={todo.date} todo={todo} editTodo={editTodo} deleteTodo={deleteTodo} completedOnChange={completedOnChange}/>
-      )}
+      {
+        todoList.length > 0 ? 
+        todoList.map((todo, i) => 
+          <TodoCard key={todo.date} todo={todo} editTodo={editTodo} deleteTodo={deleteTodo} completedOnChange={completedOnChange}/>
+        )
+      : search !== "" ? `По данному запросу ${search} ничего не найдено!!` :  "empty"
+      }
     </div>
   )
 }
